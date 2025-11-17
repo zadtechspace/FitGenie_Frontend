a6689cf0648e19995b1c02b25eadc42f29cb95ba
Zadtechspace <oppo159350@gmail.com>
2025-11-17 22:04:01 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-     tailwindcss()
+    //  tailwindcss()
   ],
 })
 
